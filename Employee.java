eef4d1a90b4b9c84d8fb24f2391eaf7fc7620450
Shubhangi Porje <112761956+Shubhangi0803@users.noreply.github.com>
2023-08-20 18:26:44 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -9,7 +9,6 @@
 
 import java.time.LocalDate;
 import java.util.Scanner;
-
 public class Employee
 {
     public static void main(String args[])
@@ -20,9 +19,9 @@ public class Employee
         Scanner sc = new Scanner(System.in);
         
         System.out.print("Enter Employee id     : ");
-	  emp_id=sc.nextInt();
- 	  System.out.print("Enter Employee Name   : ");
-	  emp_name=sc.next();
+	emp_id=sc.nextInt();
+ 	System.out.print("Enter Employee Name   : ");
+	emp_name=sc.next();
         System.out.print("Enter your data of birth yyyy-mm-dd format :");
         dob = sc.next();
         LocalDate birthDate = LocalDate.parse(dob);
@@ -30,10 +29,10 @@ public class Employee
         System.out.print("Enter the basic payment between 4500 and 6500:");
         basic = sc.nextDouble();
         DA=basic*0.2;
-	  TA=basic*0.1;
-	  HRA=basic*0.3;
+	TA=basic*0.1;
+	HRA=basic*0.3;
         PF=(basic+DA)*0.5;
-	  salary=basic+DA+HRA;
+	salary=basic+DA+HRA;
         if(basic<4500 || basic>6500)
         {
             if(basic < 4500)
@@ -47,27 +46,27 @@ public class Employee
                 return;
             }
         }
-            System.out.println("\n----- Employee Details ----\n");
-		System.out.println("EMPID              : "+emp_id);
-		System.out.println("Employee Name      : "+emp_name);
-	      System.out.println("Date of birth      : "+birthDate);
-		System.out.println("Basic Salary       : "+basic);
-		System.out.println("Dearness Allow     : "+DA);
-		System.out.println("Travel Allow       : "+TA);
-		System.out.println("HouseRent Allow    : "+HRA);
-		System.out.println("Net Salary Pay     : "+salary);
-		System.out.println("Provident Fund     : "+PF);
-		System.out.println("Retirement Date    : "+retirementDate);
-		if(salary>10000)
-		{
-			System.out.println("\nEmployee is eligible for 1000 RS deduction of professional tax.");
-			deduct_amount=salary-1000;
-			System.out.println("\nSalary after deduction :"+deduct_amount);
-		}
-		else
-		{
-			System.out.println("Employee is not eligible for 1000 rs. deduction of professional tax.");
-		}
-		sc.close();
+        System.out.println("\n----- Employee Details ----\n");
+	System.out.println("EMPID              : "+emp_id);
+	System.out.println("Employee Name      : "+emp_name);
+	System.out.println("Date of birth      : "+birthDate);
+	System.out.println("Basic Salary       : "+basic);
+	System.out.println("Dearness Allow     : "+DA);
+	System.out.println("Travel Allow       : "+TA);
+	System.out.println("HouseRent Allow    : "+HRA);
+	System.out.println("Net Salary Pay     : "+salary);
+	System.out.println("Provident Fund     : "+PF);
+	System.out.println("Retirement Date    : "+retirementDate);
+	if(salary>10000)
+	{
+		System.out.println("\nEmployee is eligible for 1000 RS deduction of professional tax.");
+		deduct_amount=salary-1000;
+		System.out.println("\nSalary after deduction :"+deduct_amount);
+	}
+	else
+	{
+		System.out.println("Employee is not eligible for 1000 rs. deduction of professional tax.");
+	}
+	sc.close();
     }
 }
